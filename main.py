cd66ed5fcb82309113ea8c6ee8fa763be54efb03
federico Lubbert <federico.lubbert@gmail.com>
2024-03-13 18:28:17 +0100

    init

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+USERNAME = "cornholio"
+TOKEN = "jsdlfenuff"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "jsdlfenuff",
+    "username": "cornholio",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "pages",
+    "type": "int",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
